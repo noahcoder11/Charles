8a62b0986ac3d449875770223ee4bad6fbad68e0
Caleb Hester <naclcaleb@gmail.com>
2024-05-05 19:24:05 -0700

    gjyhfdsfko;pl'ko;jilhkugjyfhtdg

--- modules/brocas_area.py
+++ modules/brocas_area.py
@@ -9,13 +9,13 @@ class BrocasArea:
         self.client = OpenAI(api_key=config.PROJECT_CONFIG['OPENAI_API_KEY'])
 
     def speak(self, text):
-        audio = self.client.audio.speech.with_streaming_response.create(
+        with self.client.audio.speech.with_streaming_response.create(
             model='tts-1',
             voice='onyx',
             response_format='mp3',
             input=text
-        )
-        audio.stream_to_file('speech.mp3')
-        data, fs = sf.read('speech.mp3')
-        sd.play(data, fs)
-        sd.wait()
+        ) as audio:
+            audio.stream_to_file('speech.mp3')
+            data, fs = sf.read('speech.mp3')
+            sd.play(data, fs)
+            sd.wait()
