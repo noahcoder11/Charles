7314030e7666ff0cfda1424cd80b79db1aeead67
Caleb Hester <naclcaleb@gmail.com>
2024-05-05 18:18:00 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
-from modules.wakeword import Wakeword
-from modules.audio import Audio
+from modules import *
 
 wakeword_detector = Wakeword()
 audio = Audio(wakeword_detector.porcupine.sample_rate, wakeword_detector.porcupine.frame_length)
+camera = Camera(0)
+wernickes_area = WernickesArea()
+generator = Generator()
+brocas_area = BrocasArea()
+
 audio.create_stream()
 
-while(True):
+camera.start_capture()
+
+while True:
     frame = audio.get_next_frame()
 
-    if(wakeword_detector.process(frame)):
-       print("Wakeword Detected")
-       break
+    if wakeword_detector.process(frame):
+        print("Wakeword Detected")
+        
+        #Take a picture
+        imageBase64 = camera.get_snapshot_base64()
+
+        #Now do speech recognition
+        recognized_text = wernickes_area.get_recognized_text()
+
+        #Now generate a response
+        response = generator.generate_response(recognized_text, imageBase64)
+
+        #Now speak the response
+        brocas_area.speak(response)
+
+
 
 wakeword_detector.cleanup()
+camera.cleanup()
--- modules/__init__.py
+++ modules/__init__.py
@@ -2,3 +2,5 @@ from .generator import Generator
 from .wernickes_area import WernickesArea
 from .brocas_area import BrocasArea
 from .wakeword import Wakeword
+from .audio import Audio
+from .camera import Camera
