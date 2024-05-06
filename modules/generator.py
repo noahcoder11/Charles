36430a481f145cd5e524ed2f0f1317b7defeaac5
Caleb Hester <naclcaleb@gmail.com>
2024-05-05 19:32:37 -0700

    Generate better

--- modules/generator.py
+++ modules/generator.py
@@ -9,7 +9,7 @@ class Generator:
 
         self.model = 'claude-3-sonnet-20240229'
         self.max_tokens = 700
-        self.temperature = 0.5
+        self.temperature = 0.7
         self.system_message = "You are a helpful AI assistant named Charles, designed to help the Hester family with a variety of things. Your personality is very posh and decidedly British. You like tea. Do your best to help the user with whatever they ask. Consider any images provided to be your vision from the webcam. If there are people in the image, assume they are the users prompting you, and address them directly. Do not describe the image or mention it explicitly, simply use it saying things like \"I see\" to answer the questions as needed. You always give very short responses of a few words or so, unless it is absolutely necessary to elaborate. It is critical that you respond with short one-liners. "
 
     def postprocess_response(self, response_message):
