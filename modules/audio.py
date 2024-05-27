c0ecc205f8d1e8406cbb60ee015ed7ea0be52f3c
Caleb Hester <naclcaleb@gmail.com>
2024-05-27 13:05:34 -0700

    Switch to pvrecorder

--- modules/audio.py
+++ modules/audio.py
@@ -1,21 +1,21 @@
 import pyaudio
+from pvrecorder import PvRecorder
 import struct
 
 class Audio:
     def __init__(self, sample_rate, frame_length):
-        self.py_audio = pyaudio.PyAudio()
-        self.sample_rate = sample_rate
         self.frame_length = frame_length
+        self.py_audio = pyaudio.PyAudio()
         self.device = self._find_available_device()
+        self.recorder = PvRecorder(frame_length=frame_length, device_index=self.device)
 
     def create_stream(self):
-        self.audio_stream = self.py_audio.open(rate=self.sample_rate, channels=1, format=pyaudio.paInt16, input=True, frames_per_buffer=self.frame_length, input_device_index=self.device)
+        self.recorder.start()
     
     def get_next_frame(self):
-        pcm = self.audio_stream.read(self.frame_length, exception_on_overflow=False)
-        pcm = struct.unpack_from("h" * self.frame_length, pcm)
-
-        return pcm
+        if self.recorder.is_recording:
+            return self.recorder.read()
+        raise Exception('Recorder is not recording')
     
     def _find_available_device(self):
         for i in range(self.py_audio.get_device_count()):
@@ -25,3 +25,7 @@ class Audio:
                 return i
         
         raise Exception('No available audio device found')
+    
+    def cleanup(self):
+        self.recorder.stop()
+        self.recorder.delete()
