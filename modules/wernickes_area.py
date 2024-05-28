701eb7973d1da8101dd02190f66731761767461c
Caleb Hester <naclcaleb@gmail.com>
2024-05-28 12:37:53 -0700

    Fix device available conflicts?

--- modules/wernickes_area.py
+++ modules/wernickes_area.py
@@ -14,30 +14,29 @@ class WernickesArea:
         self.recognizer.dynamic_energy_threshold = False
         self.source = sr.Microphone(input_device_index)
         self.output_device_index = output_device_index
-        self.source.__enter__()
         self.client = OpenAI(api_key=config.PROJECT_CONFIG['OPENAI_API_KEY'])
 
     def get_recognized_text(self):
         text = ''
-        print("Listening...")
-        playsound('./sounds/Charles_Alert.wav', self.output_device_index)
-        
-        #self.recognizer.adjust_for_ambient_noise(source=source)
-        audio = self.recognizer.listen(self.source)
-
-        print('Audio captured')
-        playsound('./sounds/Charles_end.wav', self.output_device_index)
-
-        try:
-            text = self.recognizer.recognize_whisper_api(audio, api_key=config.PROJECT_CONFIG['OPENAI_API_KEY'])
+        with self.source as source:
+            print("Listening...")
+            playsound('./sounds/Charles_Alert.wav', self.output_device_index)
             
-            print("Transcribed")
-        except:
-            raise Exception("Could not recognize audio")
+            #self.recognizer.adjust_for_ambient_noise(source=source)
+            audio = self.recognizer.listen(source)
+
+            print('Audio captured')
+            playsound('./sounds/Charles_end.wav', self.output_device_index)
+
+            try:
+                text = self.recognizer.recognize_whisper_api(audio, api_key=config.PROJECT_CONFIG['OPENAI_API_KEY'])
+                
+                print("Transcribed")
+            except:
+                raise Exception("Could not recognize audio")
             
         return text
     
     def __del__(self):
-        self.source.__exit__(None, None, None)
         self.client.__exit__(None, None, None)
         del self.recognizer
